b870a0f32fe0b17167ce32c2ef0fb5cbf790572d
Pamento <pamento.dev@gmail.com>
2021-07-07 01:21:49 +0200

    Add fragments with xml layouts.

---
+++ app/src/main/java/com/openclassrooms/realestatemanager/ui/activity/MainActivity.java
@@ -0,0 +1,44 @@
+package com.openclassrooms.realestatemanager.ui.activity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.openclassrooms.realestatemanager.R;
+import com.openclassrooms.realestatemanager.databinding.ActivityMainBinding;
+import com.openclassrooms.realestatemanager.ui.fragments.ListProperty;
+import com.openclassrooms.realestatemanager.ui.fragments.MapFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+        displayListFragment();
+    }
+
+    private void displayListFragment() {
+        ListProperty listProperty = ListProperty.newInstance(null, null);
+
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction fTransaction = fm
+                .beginTransaction();
+        fTransaction.add(R.id.main_activity_fragment_container, listProperty).addToBackStack(null).commit();
+    }
+
+    public void displayMap() {
+        MapFragment mf = MapFragment.newInstance(null, null);
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction transaction = fm.beginTransaction();
+        transaction.setReorderingAllowed(true);
+        transaction.replace(R.id.main_activity_fragment_container, mf);
+        transaction.commit();
+    }
+}
