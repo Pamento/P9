8befabfcac53b418b8aa16b399dd2f27c8d82af2
Pamento <pamento.dev@gmail.com>
2021-07-12 15:11:53 +0200

    Implementation of dependencies for Room Database.

--- app/src/main/java/com/openclassrooms/realestatemanager/data/model/SingleProperty.java
+++ app/src/main/java/com/openclassrooms/realestatemanager/data/model/SingleProperty.java
@@ -1,8 +1,29 @@
 package com.openclassrooms.realestatemanager.data.model;
 
 
+import androidx.room.PrimaryKey;
+
 public class SingleProperty {
-    String id;
+
+    @PrimaryKey
+    public String id;
+    public String type;
+    public String description;
+    public int surface;
+    public long price;
+    public int rooms;
+    public int bedroom;
+    public int bathroom;
+    public long dateInit;
+    public long dateSold;
+    public String address1;
+    public String address2;
+    public String city;
+    public String quarter;
+    public int postalCode;
+    public String amenities;
+    public String agent;
+
 
     public SingleProperty(String id) {
         this.id = id;
---
+++ app/src/main/java/com/openclassrooms/realestatemanager/data/model/local/database/PropertyDatabase.java
@@ -0,0 +1,7 @@
+package com.openclassrooms.realestatemanager.data.model.local.database;
+
+import androidx.room.RoomDatabase;
+
+
+public abstract class PropertyDatabase extends RoomDatabase {
+}
